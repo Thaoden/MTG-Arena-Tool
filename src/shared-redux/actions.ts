93a39e303d2e8752820fb690a7a6e8c92cc23021
Thaoden <Thaoden@users.noreply.github.com>
2020-05-24 01:02:00 +0200

    Added type ActionsArg to actions.ts, modelling all possible reducer calls and their corresponding data types

--- src/shared-redux/actions.ts
+++ src/shared-redux/actions.ts
@@ -15,78 +15,436 @@ import * as DraftsSlice from "./slices/draftsSlice";
 import * as SeasonalSlice from "./slices/seasonalSlice";
 import * as DeckChangesSlice from "./slices/deckChangesSlice";
 import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
+import { InternalMatch } from "../types/match";
+import { InternalEvent } from "../types/event";
+import { InternalDeck, DeckChange } from "../types/Deck";
+import { InternalEconomyTransaction } from "../types/inventory";
+import { InternalDraft } from "../types/draft";
+import { SeasonalRankData } from "../types/Season";
 
-export type Actions =
-  | "SET_SETTINGS"
-  | "SET_APP_SETTINGS"
-  | "SET_ARCHIVED"
-  | "SET_BACK_COLOR"
-  | "SET_BACK_GRPID"
-  | "SET_BACK_IMAGE"
-  | "SET_LOADING"
-  | "SET_NO_LOG"
-  | "SET_OFFLINE"
-  | "SET_PATREON"
-  | "SET_POPUP"
-  | "SET_SHARE_DIALOG"
-  | "SET_SHARE_DIALOG_OPEN"
-  | "SET_SHARE_DIALOG_URL"
-  | "SET_NAV_INDEX"
-  | "SET_SUBNAV"
-  | "SET_TOPARTIST"
-  | "SET_TOPNAV"
-  | "SET_UPDATE_STATE"
-  | "SET_SYNC_STATE"
-  | "SET_TO_PUSH"
-  | "SET_HOVER_IN"
-  | "SET_HOVER_OUT"
-  | "SET_CAN_LOGIN"
-  | "SET_LOGIN_EMAIL"
-  | "SET_LOGIN_FORM"
-  | "SET_LOGIN_PASSWORD"
-  | "SET_LOGIN_REMEMBER"
-  | "SET_LOGIN_STATE"
-  | "SET_HOME_DATA"
-  | "SET_BOOSTER_WIN_FACTOR"
-  | "SET_COUNT_MODE"
-  | "SET_FUTURE_BOOSTERS"
-  | "SET_MYTHIC_DRAFT_FACTOR"
-  | "SET_RARE_DRAFT_FACTOR"
-  | "SET_ACTIVE_EVENTS"
-  | "SET_EXPLORE_DATA"
-  | "SET_EXPLORE_FILTERS"
-  | "SET_EXPLORE_FILTERS_SKIP"
-  | "SET_MATCH"
-  | "SET_MANY_MATCHES"
-  | "SET_EVENT"
-  | "SET_MANY_EVENTS"
-  | "SET_PLAYERDB"
-  | "SET_APPDB"
-  | "SET_PLAYER_ID"
-  | "SET_PLAYER_NAME"
-  | "SET_ARENA_VERSION"
-  | "SET_PLAYER_ECONOMY"
-  | "SET_TAG_COLORS"
-  | "EDIT_TAG_COLOR"
-  | "SET_RANK"
-  | "ADD_CARD"
-  | "ADD_CARDS_LIST"
-  | "ADD_CARDS_KEYS"
-  | "ADD_CARDS_FROM_STORE"
-  | "REMOVE_DECK_TAG"
-  | "ADD_DECK_TAG"
-  | "SET_DECK_TAGS"
-  | "SET_DECK"
-  | "SET_MANY_DECKS"
-  | "SET_MANY_STATIC_DECKS"
-  | "SET_ECONOMY"
-  | "SET_MANY_ECONOMY"
-  | "SET_DRAFT"
-  | "SET_MANY_DRAFT"
-  | "SET_SEASONAL"
-  | "SET_MANY_SEASONAL"
-  | "SET_DECK_CHANGE"
-  | "SET_MANY_DECK_CHANGES";
+type SetSettingsArg = {
+  type: "SET_SETTINGS";
+  arg: SettingsSlice.Settings;
+};
+
+type SetAppSettingsArg = {
+  type: "SET_APP_SETTINGS";
+  arg: AppSettingsSlice.AppSettings;
+};
+
+type SetArchivedSettingsArg = {
+  type: "SET_ARCHIVED";
+  arg: { id: string; archived: boolean };
+};
+
+type SetBackColorArg = {
+  type: "SET_BACK_COLOR";
+  arg: string;
+};
+
+type SetBackGrpIdArg = {
+  type: "SET_BACK_GRPID";
+  arg: number;
+};
+
+type SetBackImageArg = {
+  type: "SET_BACK_IMAGE";
+  arg: string;
+};
+
+type SetLoadingArg = {
+  type: "SET_LOADING";
+  arg: boolean;
+};
+
+type SetNoLogArg = {
+  type: "SET_NO_LOG";
+  arg: boolean;
+};
+
+type SetOfflineArg = {
+  type: "SET_OFFLINE";
+  arg: boolean;
+};
+
+type SetPatreonArg = {
+  type: "SET_PATREON";
+  arg: RendererSlice.RendererState["patreon"];
+};
+
+type SetPopupArg = {
+  type: "SET_POPUP";
+  arg: RendererSlice.RendererState["popup"];
+};
+
+type SetShareDialogArg = {
+  type: "SET_SHARE_DIALOG";
+  arg: RendererSlice.RendererState["shareDialog"];
+};
+
+type SetShareDialogOpenArg = {
+  type: "SET_SHARE_DIALOG_OPEN";
+  arg: boolean;
+};
+
+type SetShareDialogUrl = {
+  type: "SET_SHARE_DIALOG_URL";
+  arg: string;
+};
+
+type SetNavIndexArg = {
+  type: "SET_NAV_INDEX";
+  arg: number;
+};
+
+type SetSubnavArg = {
+  type: "SET_SUBNAV";
+  arg: RendererSlice.RendererState["subNav"];
+};
+
+type SetTopArtistArg = {
+  type: "SET_TOPARTIST";
+  arg: string;
+};
+
+type SetTopnavArg = {
+  type: "SET_TOPNAV";
+  arg: number;
+};
+
+type SetUpdateStateArg = {
+  type: "SET_UPDATE_STATE";
+  arg: string;
+};
+
+type SetSyncStateArg = {
+  type: "SET_SYNC_STATE";
+  arg: number;
+};
+
+type SetToPushArg = {
+  type: "SET_TO_PUSH";
+  arg: RendererSlice.RendererState["syncToPush"];
+};
+
+type SetHoverInArg = {
+  type: "SET_HOVER_IN";
+  arg: { grpId: number; wanted: number };
+};
+
+type SetHoverOutArg = {
+  type: "SET_HOVER_OUT";
+  arg: never;
+};
+
+type SetCanLoginArg = {
+  type: "SET_CAN_LOGIN";
+  arg: boolean;
+};
+
+type SetLoginEmailArg = {
+  type: "SET_LOGIN_EMAIL";
+  arg: string;
+};
+
+type SetLoginFormArg = {
+  type: "SET_LOGIN_FORM";
+  arg: LoginSlice.Login["loginForm"];
+};
+
+type SetLoginPasswordArg = {
+  type: "SET_LOGIN_PASSWORD";
+  arg: string;
+};
+
+type SetLoginRememberArg = {
+  type: "SET_LOGIN_REMEMBER";
+  arg: boolean;
+};
+
+type SetLoginStateArg = {
+  type: "SET_LOGIN_STATE";
+  arg: 1 | 2 | 3 | 4;
+};
+
+type SetHomeDataArg = {
+  type: "SET_HOME_DATA";
+  arg: HomeSlice.Home;
+};
+
+type SetBoosterWinFactorArg = {
+  type: "SET_BOOSTER_WIN_FACTOR";
+  arg: number;
+};
+
+type SetCountModeArg = {
+  type: "SET_COUNT_MODE";
+  arg: string;
+};
+
+type SetFutureBoostersArg = {
+  type: "SET_FUTURE_BOOSTERS";
+  arg: number;
+};
+
+type SetMythicDraftFactorArg = {
+  type: "SET_MYTHIC_DRAFT_FACTOR";
+  arg: number;
+};
+
+type SetRareDraftFactorArg = {
+  type: "SET_RARE_DRAFT_FACTOR";
+  arg: number;
+};
+
+type SetActiveEventsArg = {
+  type: "SET_ACTIVE_EVENTS";
+  arg: string;
+};
+
+type SetExploreDataArg = {
+  type: "SET_EXPLORE_DATA";
+  arg: ExploreSlice.Explore["data"];
+};
+
+type SetExploreFiltersArg = {
+  type: "SET_EXPLORE_FILTERS";
+  arg: ExploreSlice.ExploreQuery;
+};
+
+type SetExploreFiltersSkipArg = {
+  type: "SET_EXPLORE_FILTERS_SKIP";
+  arg: number;
+};
+
+type SetMatchArg = {
+  type: "SET_MATCH";
+  arg: InternalMatch;
+};
+
+type SetManyMatchesArg = {
+  type: "SET_MANY_MATCHES";
+  arg: InternalMatch[];
+};
+
+type SetEventArg = {
+  type: "SET_EVENT";
+  arg: InternalEvent;
+};
+
+type SetManyEventsArg = {
+  type: "SET_MANY_EVENTS";
+  arg: InternalEvent[];
+};
+
+type SetPlayerDbArg = {
+  type: "SET_PLAYERDB";
+  arg: string;
+};
+
+type SetAppdbArg = {
+  type: "SET_APPDB";
+  arg: string;
+};
+
+type SetPlayerIdArg = {
+  type: "SET_PLAYER_ID";
+  arg: string;
+};
+
+type SetPlayerNameArg = {
+  type: "SET_PLAYER_NAME";
+  arg: string;
+};
+
+type SetArenaVersionArg = {
+  type: "SET_ARENA_VERSION";
+  arg: string;
+};
+
+type SetPlayerEconomyArg = {
+  type: "SET_PLAYER_ECONOMY";
+  arg: PlayerDataSlice.PlayerData["economy"];
+};
+
+type SetTagColorsArg = {
+  type: "SET_TAG_COLORS";
+  arg: Record<string, string>;
+};
+
+type EditTagColorArg = {
+  type: "EDIT_TAG_COLOR";
+  arg: { tag: string; color: string };
+};
+
+type SetRankArg = {
+  type: "SET_RANK";
+  arg: PlayerDataSlice.PlayerData["rank"];
+};
+
+type AddCardArg = {
+  type: "ADD_CARD";
+  arg: number;
+};
+
+type AddCardsListArg = {
+  type: "ADD_CARDS_LIST";
+  arg: number[];
+};
+
+type AddCardsKeysArg = {
+  type: "ADD_CARDS_KEYS";
+  arg: { [grpId: string]: number };
+};
+
+type AddCardsFromStoreArg = {
+  type: "ADD_CARDS_FROM_STORE";
+  arg: any;
+};
+
+type RemoveDeckTagArg = {
+  type: "REMOVE_DECK_TAG";
+  arg: { tag: string; deck: string };
+};
+
+type AddDeckTagArg = {
+  type: "ADD_DECK_TAG";
+  arg: { tag: string; deck: string };
+};
+
+type SetDeckTagsArg = {
+  type: "SET_DECK_TAGS";
+  arg: Record<string, string[]>;
+};
+
+type SetDeckArg = {
+  type: "SET_DECK";
+  arg: InternalDeck;
+};
+
+type SetManyDecksArg = {
+  type: "SET_MANY_DECKS";
+  arg: InternalDeck[];
+};
+
+type SetManyStaticDecksArg = {
+  type: "SET_MANY_STATIC_DECKS";
+  arg: InternalDeck[];
+};
+
+type SetEconomyArg = {
+  type: "SET_ECONOMY";
+  arg: InternalEconomyTransaction;
+};
+
+type SetManyEconomyArg = {
+  type: "SET_MANY_ECONOMY";
+  arg: InternalEconomyTransaction[];
+};
+
+type SetDraftArg = {
+  type: "SET_DRAFT";
+  arg: InternalDraft;
+};
+
+type SetManyDraftArg = {
+  type: "SET_MANY_DRAFT";
+  arg: InternalDraft[];
+};
+
+type SetSeasonalArg = {
+  type: "SET_SEASONAL";
+  arg: SeasonalRankData;
+};
+
+type SetManySeasonalArg = {
+  type: "SET_MANY_SEASONAL";
+  arg: SeasonalRankData[];
+};
+
+type SetDeckChangeArg = {
+  type: "SET_DECK_CHANGE";
+  arg: DeckChange;
+};
+
+type SetManyDeckChangesArg = {
+  type: "SET_MANY_DECK_CHANGES";
+  arg: DeckChange[];
+};
+
+export type ActionsArg =
+  | SetSettingsArg
+  | SetAppSettingsArg
+  | SetArchivedSettingsArg
+  | SetBackColorArg
+  | SetBackGrpIdArg
+  | SetBackImageArg
+  | SetLoadingArg
+  | SetNoLogArg
+  | SetOfflineArg
+  | SetPatreonArg
+  | SetPopupArg
+  | SetShareDialogArg
+  | SetShareDialogOpenArg
+  | SetShareDialogUrl
+  | SetNavIndexArg
+  | SetSubnavArg
+  | SetTopnavArg
+  | SetTopArtistArg
+  | SetUpdateStateArg
+  | SetSyncStateArg
+  | SetToPushArg
+  | SetHoverInArg
+  | SetHoverOutArg
+  | SetCanLoginArg
+  | SetLoginEmailArg
+  | SetLoginFormArg
+  | SetLoginPasswordArg
+  | SetLoginRememberArg
+  | SetLoginStateArg
+  | SetHomeDataArg
+  | SetBoosterWinFactorArg
+  | SetCountModeArg
+  | SetFutureBoostersArg
+  | SetMythicDraftFactorArg
+  | SetRareDraftFactorArg
+  | SetActiveEventsArg
+  | SetExploreDataArg
+  | SetExploreFiltersArg
+  | SetExploreFiltersSkipArg
+  | SetMatchArg
+  | SetManyMatchesArg
+  | SetEventArg
+  | SetManyEventsArg
+  | SetPlayerDbArg
+  | SetAppdbArg
+  | SetPlayerIdArg
+  | SetPlayerNameArg
+  | SetArenaVersionArg
+  | SetPlayerEconomyArg
+  | SetTagColorsArg
+  | SetRankArg
+  | AddCardArg
+  | AddCardsListArg
+  | AddCardsKeysArg
+  | AddCardsFromStoreArg
+  | RemoveDeckTagArg
+  | AddDeckTagArg
+  | SetDeckTagsArg
+  | SetDeckArg
+  | SetManyDecksArg
+  | SetManyStaticDecksArg
+  | SetEconomyArg
+  | SetManyEconomyArg
+  | SetDraftArg
+  | SetManyDraftArg
+  | SetSeasonalArg
+  | SetManySeasonalArg
+  | SetDeckChangeArg
+  | SetManyDeckChangesArg
+  | EditTagColorArg;
+
+export type Actions = ActionsArg["type"];
 
 const actions: Record<
   Actions,
